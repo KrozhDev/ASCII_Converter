f773559c15557e70f17d94955dc53a48a65a20d6
KrozhDev <roko.mdn@gmail.com>
2023-03-10 21:51:24 +0300

    First Commit

--- src/ru/netology/graphics/Main.java
+++ src/ru/netology/graphics/Main.java
@@ -1,5 +1,7 @@
 package ru.netology.graphics;
 
+import ru.netology.graphics.image.ColorSchema;
+import ru.netology.graphics.image.Converter;
 import ru.netology.graphics.image.TextGraphicsConverter;
 import ru.netology.graphics.server.GServer;
 
@@ -8,14 +10,20 @@ import java.io.PrintWriter;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        TextGraphicsConverter converter = null; // Создайте тут объект вашего класса конвертера
-
+        TextGraphicsConverter converter = new Converter(); // Создайте тут объект вашего класса конвертера
         GServer server = new GServer(converter); // Создаём объект сервера
         server.start(); // Запускаем
 
-        // Или то же, но с выводом на экран:
-        //String url = "https://raw.githubusercontent.com/netology-code/java-diplom/main/pics/simple-test.png";
-        //String imgTxt = converter.convert(url);
-        //System.out.println(imgTxt);
+
+//        TextGraphicsConverter converter = new Converter();
+//
+//        converter.setMaxRatio(4);
+//        converter.setMaxWidth(300);
+//        converter.setMaxHeight(300);
+//
+//        // Или то же, но с выводом на экран:
+//        String url = "https://i.ibb.co/MGKNh34/test.png";
+//        String imgTxt = converter.convert(url);
+//        System.out.println(imgTxt);
     }
 }
